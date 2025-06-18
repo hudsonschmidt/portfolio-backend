6bcd8f700cc986fc7649f7167a434678ce25dad7
hudsonschmidt <hudsonschmidt08@gmail.com>
2025-06-17 18:04:36 -0600

    resume

--- src/api/resume.py
+++ src/api/resume.py
@@ -10,3 +10,23 @@ router = APIRouter(
     tags=["resume"],
     dependencies=[Depends(auth.get_api_key)],
 )
+
+@router.get("/", tags=["resume"], response_model=str)
+def get_projects() -> str:
+    """
+    Retrieves all projects and respective details.
+    """
+    with db.engine.begin() as connection:
+        result = connection.execute(
+            sqlalchemy.text(
+                """
+                SELECT link
+                FROM resume
+                ORDER BY id DESC
+                LIMIT 1
+                """
+            )
+        ).scalar_one()
+
+    
+    return result
